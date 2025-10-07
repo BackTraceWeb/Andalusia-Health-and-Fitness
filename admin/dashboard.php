c48fda7b6dd624b183a3fe5c3314979df015bbb8
BackTraceWeb <brady@back-trace.com>
2025-10-06 21:31:50 -0500

    Fix require path for bootstrap file in admin dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__ . '/../_bootstrap.php';
+require __DIR__ . '/../api/_bootstrap.php';
 header('Content-Type: text/html; charset=UTF-8');
 ?>
 <!DOCTYPE html>
