55f664e2cafdf0d72676ae7311ee252e78c1019b
BackTraceWeb <brady@back-trace.com>
2025-10-19 15:58:12 -0500

    Update Authorize.Net transaction key: replace with new production key

--- api/config.php
+++ api/config.php
@@ -3,4 +3,5 @@
 define('AUTH_ENV', 'sandbox');
 define('AUTH_API_URL', 'https://apitest.authorize.net/xml/v1/request.api');
 define('AUTH_LOGIN_ID', '9rwWD86pE79');   // ← replace
-define('AUTH_TRANSACTION_KEY', '7PKU58re2vF42bP3AZd'); // ← replace
+define('AUTH_TRANSACTION_KEY', '6TR6Dr35G637teuH');
+
