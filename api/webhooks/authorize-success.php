b1c6160d809c1c2dff6ba43a070fba105aa2c65f
BackTraceWeb <brady@back-trace.com>
2025-10-22 20:54:36 -0500

    Set production flag to false in authorize-success.php for testing purposes

--- api/webhooks/authorize-success.php
+++ api/webhooks/authorize-success.php
@@ -5,7 +5,7 @@ date_default_timezone_set('UTC');
 
 // ---- CONFIG ----
 $csvPath = __DIR__ . '/payments.csv';   // lives beside this PHP file
-$prod = true;                                    // true in production
+$prod = false;                                    // true in production
 $apiSignatureKeyHex =
  '21C260460E71D8FFC7437BA38D1938A0D8C531810250F8FC36CB06197C93776609F7A6AB937FD53DE0C0E43409B4BF6AC89F7AC1FBA77BDFA4DE9ECC535F3C7C';
 // --------------
